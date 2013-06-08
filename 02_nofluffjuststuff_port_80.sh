8797136c307b1146508a3d0c6ead09e8a3348a44
Demian Neidetcher <demian0311@gmail.com>
2013-06-08 13:40:53 -0400

    move to nfjs

--- 02_nofluffjuststuff_port_80.sh
+++ 02_nofluffjuststuff_port_80.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . spelunking.sh
 
-ACTUAL=$(nmap -p 80 neidetcher.com 2>&1)
+ACTUAL=$(nmap -p 80 nofluffjuststuff.com 2>&1)
 echo $ACTUAL | grep -q "80/tcp open http"
 
 evaluateLastStatus "port not open"
